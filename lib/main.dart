1b1288176fe0cb5be48f72a0924fa4423794dea2
erdoganabaci <erdoganabaci97@gmail.com>
2020-06-25 04:20:48 +0300

    url launcher doesnt work

--- lib/main.dart
+++ lib/main.dart
@@ -7,8 +7,13 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_cache_manager/flutter_cache_manager.dart';
 import 'package:mime/mime.dart';
+//import 'package:share_social_media_plugin/share_social_media_plugin.dart';
 import 'package:video_thumbnail/video_thumbnail.dart';
 import 'package:wc_flutter_share/wc_flutter_share.dart';
+import 'package:image_downloader/image_downloader.dart';
+import 'package:url_launcher/url_launcher.dart';
+
+
 
 void main() => runApp(MyApp());
 
@@ -41,14 +46,45 @@ class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
 
   Future<void> _incrementCounter() async {
+   var url = "https://raw.githubusercontent.com/wiki/ko2ic/image_downloader/images/flutter.png";
+    //var url = "http://erdoganabaci.site/wp-content/uploads/2020/05/cute-dog.jpg";
+    //var imageId = await ImageDownloader.downloadImage("http://erdoganabaci.site/wp-content/uploads/2020/05/cute-dog.jpg");
+    var imageId = await ImageDownloader.downloadImage(url);
 
 
+    print("This is image ID $imageId");
+    var mediaId = imageId;
+    final instagramUrl = 'instagram://library?LocalIdentifier=$mediaId';
+   //final instagramUrl = 'instagram://media?id=$mediaId';
 
+     //String url1 = 'tel:1-408-555-5555';
 
-    var request = await HttpClient().getUrl(Uri.parse('http://erdoganabaci.site/wp-content/uploads/2020/05/cute-dog.jpg'));
+   if (await canLaunch(instagramUrl)) {
+      try {
+        //final instagramUrl = 'instagram://library?LocalIdentifier=$mediaId';
+        await launch(instagramUrl);
+        //await launch(url1);
+
+      } on PlatformException {
+        // do something
+      }
+    } else {
+      throw 'Could not launch $url';
+    }
+  /*  var request = await HttpClient().getUrl(Uri.parse('http://erdoganabaci.site/wp-content/uploads/2020/05/cute-dog.jpg'));
     var response = await request.close();
     Uint8List bytes = await consolidateHttpClientResponseBytes(response);
-    await Share.file('ESYS AMLOG', 'amlog.jpg', bytes, 'image/jpg');
+    //await ShareSocialMediaPlugin.shareInstagram("hello","images/play.jpg","images/play.jpg");
+    await Share.file('ESYS AMLOG',"images/play.jpg", bytes, 'image/jpg');
+*/
+
+
+    /*var request = await HttpClient().getUrl(Uri.parse('http://erdoganabaci.site/wp-content/uploads/2020/05/cute-dog.jpg'));
+    var response = await request.close();
+    Uint8List bytes = await consolidateHttpClientResponseBytes(response);
+    await Share.file('ESYS AMLOG', 'amlog.jpg', bytes, 'image/jpg');*/
+
+
 
 
   }
@@ -83,7 +119,7 @@ class _MyHomePageState extends State<MyHomePage> {
       floatingActionButton: FloatingActionButton(
         onPressed: _incrementCounter,
         tooltip: 'Increment',
-        child: Icon(Icons.add),
+        child: Icon(Icons.share),
       ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
